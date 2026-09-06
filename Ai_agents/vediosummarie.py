88e8c8e48425e41caa0a3d37936c3d678ff09581
Darshan R <97456299+darshan12345678910@users.noreply.github.com>
2026-09-06 10:00:27 +0530

    security: remove hardcoded Google Gemini API key and use environment variable

--- Ai_agents/vediosummarie.py
+++ Ai_agents/vediosummarie.py
@@ -13,15 +13,16 @@ import tempfile
 from dotenv import load_dotenv
 load_dotenv()
 
+import os
 from google import genai
 
-client = genai.Client(api_key="GEMINI_API_KEY")
+client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
 
 img_path = "F:\Enhanced_Vision_Aid\Testing-images\istockphoto-656497862-612x612.jpg"
 file_ref = client.files.upload(file=img_path)
 print(f'{file_ref=}')
 
-client = genai.Client(api_key="AIzaSyDJjjvm24IYc19EiiK_zEbuRax0ej2G3sA")
+client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
 response = client.models.generate_content(
     model="gemini-2.0-flash-exp",
     contents=["What can you tell me about these instruments?",
